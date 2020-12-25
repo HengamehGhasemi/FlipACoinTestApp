10e42ce3afaeeacb86171ec176788b8ed7ae3722
hengameh <work.ghasemi@gmail.com>
2020-12-25 13:12:52 +0330

    change ic luncher

--- app/src/main/java/com/hichapp/flipcoin/getRandomNumber.kt
+++ app/src/main/java/com/hichapp/flipcoin/getRandomNumber.kt
@@ -3,6 +3,8 @@ package com.hichapp.flipcoin
 import java.util.*
 
 fun getRandomNumber() : Int {
+    val max = 50
+    val min = 1
     val random = Random()
-    return random.nextInt(50)
+    return random.nextInt((max - min) + 1) + min
 }
